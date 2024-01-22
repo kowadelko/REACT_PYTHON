dbd30ecf60aef9e89e6868b7badaad3dcce2ab13
Michal Kowalski <kowalski071@gmail.com>
2024-01-22 12:57:40 +0100

    header implementation

--- my-new-app/src/App.js
+++ my-new-app/src/App.js
@@ -1,7 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+
 function App() {
   return (
     <div>
-      <h1>Vamos a la playa !!!</h1>
+      <Header />
     </div>
   );
 }
---
+++ my-new-app/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <Navbar bg="dark" data-bs-theme="dark">
+          <Navbar.Brand href="/">DACHSHUNDS ON TOUR</Navbar.Brand>
+        </Navbar>
+    )
+};
+
+export default Header;
