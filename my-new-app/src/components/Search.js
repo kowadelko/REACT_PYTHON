ffe84960ddc295b1ad92fcac8a8f2d0c47c343ad
Michal Kowalski <kowalski071@gmail.com>
2024-01-23 14:30:50 +0100

    changes many of them

--- my-new-app/src/components/Search.js
+++ my-new-app/src/components/Search.js
@@ -1,10 +1,26 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-const Search = () => {
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+
+const Search = ({word, setWord, handleSubmit}) => {
     return (
-        <Container>
-            <Row>
-                <Col>Search</Col>
+        <Container className="mt-4">
+            <Row className="justify-content-center">
+                <Col xs={12} md={8} lg={6}>
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Row>
+                            <Col xs={9}>
+                                <Form.Control 
+                                    type="text"
+                                    value={word}
+                                    onChange={(e) => setWord(e.target.value)}
+                                    placeholder="Search for new image..." />
+                            </Col>
+                            <Col>
+                                <Button variant="primary" type="submit">Submit</Button>
+                            </Col>
+                        </Form.Row>
+                    </Form>
+                </Col>
             </Row>
         </Container>
     );
