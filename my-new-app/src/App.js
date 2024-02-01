e4497d54e9002a0d45e24a70eb360a7d89a6f683
Michal Kowalski <kowalski071@gmail.com>
2024-02-01 12:26:05 +0100

    key api added

--- my-new-app/src/App.js
+++ my-new-app/src/App.js
@@ -1,33 +1,33 @@
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Search from './components/Search';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
 
 const UNSPLASHED_KEY = process.env.REACT_APP_UNSPLASHED_KEY;
 
 function App() {
-  const [word, setWord] = useState('');
+  const [word, setWord] = useState("");
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASHED_KEY}`)
-      .then((res)  => res.json())
-      .then((data)=> {
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASHED_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
         console.log(data);
       })
       .catch((err) => {
         console.log(err);
-      })
-    setWord('');
-    
-  }
+      });
+    setWord("");
+  };
 
-  
   return (
     <div>
-      <Header title= "perros salchicha on travel"/>
-      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/> 
+      <Header title="perros salchicha on travel" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
